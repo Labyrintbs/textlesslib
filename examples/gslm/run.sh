4f742db74712e20691071a2a98ad32ae3002c43c
Labyrintbs <fhm765770648@gmail.com>
2022-08-08 06:16:31 +0000

    add gslm generation runfile

--- examples/gslm/run.sh
+++ examples/gslm/run.sh
@@ -1,3 +1,9 @@
 #!bin/bash
 echo 'run textless test'
-python sample.py --language-model-data-dir /home/si/intern/fang_hongming/LM/hubert100_lm --input-file 1089-134686-0000.flac --output-file output_new.wav --prompt-duration-sec 3 --temperature 0.7 --vocab-size 100
+python sample.py \
+	--language-model-data-dir /home/si/intern/fang_hongming/LM/hubert100_lm \
+   	--input-file resample_audios/re_Vert.wav \
+   	--output-file regeneration_audios/ge_Vert.wav \
+	--prompt-duration-sec 3 \
+	--temperature 0.7 \
+	--vocab-size 100
